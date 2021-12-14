11b2a3d395695932629370802c0541c0b8c4aa41
Justin Vargas <90952647+JustinVargas0915@users.noreply.github.com>
2021-12-14 17:38:13 +0700

    deploy

--- src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
@@ -8,10 +8,7 @@ package com.csa.samplefullstack.controller;
 import com.csa.samplefullstack.entity.Employee;
 import com.csa.samplefullstack.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +27,10 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
-        
+    }
+
+    @PostMapping("/employees")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 }
