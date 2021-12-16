345eb12f06c3ae880856a47eb3e06f4331e30e2e
Justin Vargas <90952647+JustinVargas0915@users.noreply.github.com>
2021-12-16 19:29:59 +0700

    commit

--- src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
@@ -8,7 +8,19 @@ package com.csa.samplefullstack.controller;
 import com.csa.samplefullstack.entity.Employee;
 import com.csa.samplefullstack.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 import javax.persistence.Id;
 import java.util.List;
@@ -35,12 +47,19 @@ public class EmployeeController {
         return employeeRepository.save(employee);
     }
 
-    @GetMapping("/employees/{id}")
-    public Employee getEmployeeFromId(@PathVariable Long id){
-        return employeeRepository.findById(id).get();
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeFromId(@PathVariable Long employeeId){
+        return employeeRepository.findById(employeeId).get();
     }
+
     @PutMapping("/employees")
     public Employee putEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    @DeleteMapping("/employee/{employeeId}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable Long employeeId){
+        employeeRepository.deleteById(employeeId);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/csa/samplefullstack/entity/Employee.java
+++ src/main/java/com/csa/samplefullstack/entity/Employee.java
@@ -21,52 +21,52 @@ public class Employee {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long employeeId;
+    private Long employee_Id;
     public String name;
     private String department;
     private String location;
     
-    public Employee(Long employeeID, String name, String department, String location)
+    public Employee(Long employee_Id, String name, String department, String location)
     {
-        this.employeeId = employeeId;
+        this.employee_Id = employee_Id;
         this.name = name;
         this.department = department;
         this.location = location;
     }
     
     public Employee(){
-        
+
     }
-    
+
     public Long getEmployeeId(){
-        return employeeId;
+        return employee_Id;
     }
-    
-    public void setEmployeeId(Long employeeId)
+
+    public void setEmployeeId(Long employee_Id)
     {
-        this.employeeId = employeeId;
+        this.employee_Id = employee_Id;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public String getDepartment() {
         return department;
     }
-    
+
     public void setDepartment(String department) {
         this.department = department;
     }
-    
+
     public String getLocation() {
         return location;
     }
-    
+
     public void setLocation(String location) {
         this.location = location;
     }
